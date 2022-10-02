cb5d6780081c7d653c5d365e6840fab3910531c5
BinderAndreas <andreas.binder93@gmail.com>
2022-10-02 17:48:27 +0200

    second commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,19 @@
 
 import './App.css';
+import Header from './Header'
+import Footer from './Footer';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div className="container">
+      <h1>Hello world</h1>
+      <Header/>
+      <Footer/>
+      <div>
+      <h1>Hi, second divs</h1>
     </div>
+    </div>
+    
   );
 }
 
